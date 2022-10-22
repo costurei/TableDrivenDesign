377ac154acced12892f5073320ad0c5ed9568b78
Marcelo Dias <mardsystems@gmail.com>
2022-10-22 11:40:46 -0300

    task: adiciona projeto web api

---
+++ src/MinhaLoja.WebApi/Controllers/PedidosController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MinhaLoja.Data;
+using MinhaLoja.Models;
+
+namespace MinhaLoja.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidosController : ControllerBase
+    {
+        private readonly MinhaLojaDbContext _db;
+
+        public PedidosController(MinhaLojaDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: api/Pedidos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pedido>>> GetPedidos()
+        {
+            return await _db.Pedidos.ToListAsync();
+        }
+
+        // GET: api/Pedidos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pedido>> GetPedido(int id)
+        {
+            var pedido = await _db.Pedidos.FindAsync(id);
+
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return pedido;
+        }
+
+        // PUT: api/Pedidos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPedido(int id, Pedido pedido)
+        {
+            if (id != pedido.Id)
+            {
+                return BadRequest();
+            }
+
+            _db.Entry(pedido).State = EntityState.Modified;
+
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PedidoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pedidos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Pedido>> PostPedido(Pedido pedido)
+        {
+            _db.Pedidos.Add(pedido);
+            await _db.SaveChangesAsync();
+
+            return CreatedAtAction("GetPedido", new { id = pedido.Id }, pedido);
+        }
+
+        // DELETE: api/Pedidos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePedido(int id)
+        {
+            var pedido = await _db.Pedidos.FindAsync(id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            _db.Pedidos.Remove(pedido);
+            await _db.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PedidoExists(int id)
+        {
+            return _db.Pedidos.Any(e => e.Id == id);
+        }
+    }
+}
